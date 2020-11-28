65d401caafdc894845725bae689c0964832b75a3
PalaHz <andrespala2000@gmail.com>
2020-11-28 18:57:32 -0500

    Datos de conexion agregados

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'tryffer'
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b8e2ac4f444cd7',
+    password: '47fa8a16',
+    database: 'heroku_f224201d36d4da4'
 });
+
 
 mysqlConnection.connect(function (err){
     if(err) {
