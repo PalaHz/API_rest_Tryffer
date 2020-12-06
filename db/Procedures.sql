04176889a4e34ae15ac552818d74b7355b572534
PalaHz <andrespala2000@gmail.com>
2020-12-05 22:20:17 -0500

    Procedimientos Agregados

--- db/Procedures.sql
+++ db/Procedures.sql
@@ -1,8 +1,8 @@
-USE `tryffer`;
+USE `heroku_86fa010ccbe436d`;
 DROP procedure IF EXISTS `usuarioAddOrEdit`;
 
 DELIMITER $$
-USE `tryffer`$$
+USE `heroku_86fa010ccbe436d`$$
 CREATE PROCEDURE usuarioAddOrEdit (
 	IN _idusuario VARCHAR(45),
     IN _nombre VARCHAR(60),
@@ -20,11 +20,11 @@ BEGIN
 END$$
 DELIMITER ;
 
-USE `tryffer`;
+USE `heroku_86fa010ccbe436d`;
 DROP procedure IF EXISTS `representanteAddOrEdit`;
 
 DELIMITER $$
-USE `tryffer`$$
+USE `heroku_86fa010ccbe436d`$$
 CREATE PROCEDURE representanteAddOrEdit (
 	IN _idrepresentante VARCHAR(20),
     IN _nombre VARCHAR(45),
@@ -43,3 +43,33 @@ END$$
 
 DELIMITER ;
 
+
+USE `heroku_86fa010ccbe436d`;
+DROP procedure IF EXISTS `heroku_86fa010ccbe436d`.`categoriaAddOrEdit`;
+;
+
+DELIMITER $$
+USE `heroku_86fa010ccbe436d`$$
+CREATE DEFINER=`b5a6f22084c6df`@`%` PROCEDURE `categoriaAddOrEdit`(
+IN _idcategoria INT,
+    IN _nombre VARCHAR(45),
+    IN _imgCategoria VARCHAR(100),
+    IN _descripcion VARCHAR(45)
+)
+BEGIN
+    IF _idcategoria = 0 THEN
+    INSERT INTO categoria( nombre, imgCategoria, descripcion)
+    VALUES ( _nombre, _imgCategoria, _descripcion);
+    ELSE
+    UPDATE categoria
+    SET
+    nombre = _nombre,
+    imgCategoria = _imgCategoria,
+    descripcion = _descripcion
+    WHERE idcategoria = _idcategoria;
+    END IF;
+    SELECT _idcategoria AS idcategoria;
+END$$
+
+DELIMITER ;
+;
